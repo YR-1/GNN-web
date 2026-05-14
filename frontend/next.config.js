ce81ebfdd72b2edb581f1e4f78dd5c2c67034356
yun <ylie0027@student.monash.edu>
2026-05-14 17:27:27 +0800

    fix: migrate to Next.js 16 - remove swcMinify, rename middleware to proxy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,14 @@ const distDir = process.env.NEXT_DIST_DIR
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  serverExternalPackages: [
+    'plotly.js',
+    'plotly.js-dist-min',
+    'plotly.js/lib/core',
+    'plotly.js/lib/heatmap',
+    'plotly.js/lib/scatter',
+    'react-plotly.js',
+  ],
   ...(distDir ? { distDir } : {}),
 }
 
